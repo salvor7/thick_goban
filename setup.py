db0d492b728178be8dc8b8c05487b18b5514ba05
Andrew Brown <salvor7@gmail.com>
2016-06-04 23:46:36 -0700

    Create initial package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='thick_goban',
+      version='0.1.0',
+      description='A "think" representation of a Go game position which stores "discoveries" made when changing state for new moves.',
+      url='http://github.com/salvor7/thick_goban',
+      author='Andrew AH Brown',
+      author_email='salvor7@gmail.com',
+      license='MIT',
+      packages=['thick_goban'],
+      zip_safe=False)
